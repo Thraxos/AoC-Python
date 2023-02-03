4bd4bd16344453b34c4051825f05cc6cb44e9286
Thraxos <marcus.lindellbiehl@gmail.com>
2023-02-03 15:59:14 +0100

    Update day9.py

--- AoC 2015/Day 9/day9.py
+++ AoC 2015/Day 9/day9.py
@@ -1,10 +1,12 @@
 import os
 import sys
+from itertools import permutations
 with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
     lines = f.readlines()
 f.close()
 
 graph = {}
+graph["Dummy"] = []
 
 for line in lines:
     line = line.rstrip()
@@ -13,12 +15,13 @@ for line in lines:
     if loc1 not in graph:
         graph[loc1] = [[loc2, int(distance)]]
         graph[loc1].append(["Dummy", 0])
+        graph["Dummy"].append([loc1,0])
     else:
         graph[loc1].append([loc2, int(distance)])
     if loc2 not in graph:
         graph[loc2] = [[loc1, int(distance)]]
         graph[loc2].append(["Dummy", 0])
+        graph["Dummy"].append([loc2,0])
     else:
         graph[loc2].append([loc1, int(distance)])
 
-print(graph)
