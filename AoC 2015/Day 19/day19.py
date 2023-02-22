4b1e241837cc8d78fefb5bf481c33dcd679c7dc3
Thraxos <marcus.lindellbiehl@gmail.com>
2023-02-22 16:32:55 +0100

    Day 19 done

--- AoC 2015/Day 19/day19.py
+++ AoC 2015/Day 19/day19.py
@@ -23,4 +23,16 @@ for line in lines:
 
 print(f"The number of distincts molecules that can be created is: {len(replacements)}")
 
-
+current_molecule = molecule
+steps = 0
+while current_molecule != "e":
+    for line in lines:
+        line = line.rstrip()
+        line = line.split(" => ")
+        replace_this, with_this = line
+        positions = [i for i in range(len(current_molecule)) if current_molecule.startswith(with_this, i)]
+        while len(positions) > 0:
+            current_molecule = current_molecule[0:positions[0]] + replace_this + current_molecule[positions[0]+len(with_this):]
+            steps += 1
+            positions = [i for i in range(len(current_molecule)) if current_molecule.startswith(with_this, i)]
+print(steps)
